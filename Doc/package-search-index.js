ec8b00fd76ea9e8703526ab4adbbfc880a931958
Софья  К. <58175476+SofiFairyTell@users.noreply.github.com>
2025-01-26 23:25:59 +0300

    upd

---
+++ Doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"Core"},{"l":"readConfiguration"},{"l":"TestSearch"},{"l":"TestUser"}];updateSearchResults();
--- src/test/java/TestSearch/PageTest.java
+++ src/test/java/TestSearch/PageTest.java
@@ -4,7 +4,7 @@ import Core.BaseSelenium;
 import org.junit.Test;
 
 public class PageTest extends BaseSelenium {
-  //  @Test
+    @Test
     public void UserSearch(){
       MainPage mainPage = new MainPage().StartSearch();
     }
